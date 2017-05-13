168da4a6620a4ce225426979d3469788a8f05fff
christopher-hague <christopher.a.hague@gmail.com>
2017-05-13 18:36:05 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//Code React element here
+// create a div with a className="me"
+  // inside the div insert an <h1>, a <p>, and a <ul> with a className=" me__interests"
+    // within the <ul>, insert 4 <li>'s
+
+const myH1 = React.createElement("h1", {}, "An Awesome Person")
+const myP = React.createElement("p", {}, "Who is learning React")
+const myUl = React.createElement("ul", { className: "me__interests"}, [
+  React.createElement("li", {}, "JavaScript"),
+  React.createElement("li", {}, "React"),
+  React.createElement("li", {}, "Movies"),
+  React.createElement("li", {}, "Ice cream")
+])
+var meInReact = React.createElement("div", { className: "me" }, [myH1, myP, myUl])
+
+ReactDOM.render(meInReact, document.getElementById('main'))
