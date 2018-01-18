7313e037b58a97b6aeb65ddabeddb5932243e9bb
djoseph1250 <djoseph1250@gmail.com>
2018-01-18 14:01:21 -0500

    completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+var meInReact = (
+  <div className = 'me'>
+    <h1>An Awesome Person</h1>
+    <p>Who is learning React</p>
+
+    <ul className='my-interests'>
+      <li>JavaScript</li>
+      <li>React</li>
+      <li>Movies</li>
+      <li>Ice cream</li>
+    </ul>
+  </div>
+);
+
 // IMPORTANT!!!! uncomment the lines below to make your tests work
-/*
+
 ReactDOM.render(
   meInReact,
   document.getElementById('global')
 );
 
 export default meInReact
-*/
+
